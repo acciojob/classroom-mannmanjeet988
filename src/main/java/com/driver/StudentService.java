e59c446e68cd2d6127c09ecdab769d23d4981eb0
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-04-01 20:46:19 +0530

    Changes 1

--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -1,10 +1,12 @@
 package com.driver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     @Autowired
